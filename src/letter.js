e64a952cee58a6b54e8e023b9df703e6ae26ab86
sevenflash <sevenflash@yandex.ru>
2019-05-30 21:32:30 +0430

    add letter service

---
+++ src/letter.js
@@ -0,0 +1,56 @@
+const cryptoHash = require('crypto-hashing')
+
+const hash = (buffer) => '0x' + cryptoHash('hash256', buffer).toString('hex')
+
+const Letter = (contract) => {
+  let messages = {}
+
+  const startLetter = (message, author) => {
+    const messageHash = hash(message)
+
+    messages[messageHash] = { message, author, link: null }
+
+    return messageHash
+  }
+
+  const appendMessage = (message, link, author) => {
+    const messageHash = hash(message)
+
+    messages[messageHash] = { message, author, link }
+
+    return messageHash
+  }
+
+  const claimReceipt = async (messageHash, recipient) => {
+    const { author, link } = messages[messageHash]
+
+    if (link) {
+      const { message } = messages[link]
+
+      const { tx } = await contract.revealMessage(author, messageHash, message)
+
+      return tx
+    }
+  }
+
+  const getMessages = async (owner) => {
+    const tokens = await contract.getTokens(owner)
+
+    const messages = await Promise.all(
+      tokens.map(
+        token => contract.tokenURI(token)
+      )
+    )
+
+    return messages
+  }
+
+  return {
+    startLetter,
+    appendMessage,
+    claimReceipt,
+    getMessages
+  }
+}
+
+module.exports = Letter
---
+++ test/letter.test.js
@@ -0,0 +1,51 @@
+const { artifacts, ethereum } = require("@nomiclabs/buidler");
+const { describe } = require('riteway')
+const R = require('ramda')
+const LetterChain = require('../src/letter')
+
+const contract = (() => {
+  let hashes = {}
+  let messages = {}
+
+  const revealMessage = (to, hash, message) => {
+    if (!hashes[to]) hashes[to] = []
+    hashes[to].push(hash)
+    messages[hash] = message
+    return { tx: 'transaction-hash' }
+  }
+
+  const getTokens = (owner) => hashes[owner]
+
+  const tokenURI = (hash) => messages[hash]
+
+  return { revealMessage, getTokens, tokenURI }
+})()
+
+describe('Letter', async assert => {
+  const letter = LetterChain(contract)
+
+  const [ alice, bob, calvin, deckart, service ] = await ethereum.send("eth_accounts")
+
+  const aliceMessage = 'alice-secret-message'
+  const bobMessage = 'bob-secret-message'
+
+  // Alice create a new message
+  const aliceMessageHash = await letter.startLetter(aliceMessage, alice)
+
+  // Bob claims receipt of the message from Alice
+  await letter.claimReceipt(aliceMessageHash, bob)
+
+  // Bob crafts his own message
+  const bobMessageHash = await letter.appendMessage(bobMessage, aliceMessageHash, bob)
+
+  // Calvin claims receipt of Bob's message
+  await letter.claimReceipt(bobMessageHash, calvin)
+
+  // Service has minted token for Bob revealing the message of Alice
+  assert({
+    given: 'Calvin claimed receipt of message from Bob',
+    should: 'Token revealing message of Alice minted for Bob',
+    actual: await letter.getMessages(bob),
+    expected: [ aliceMessage ]
+  })
+})
